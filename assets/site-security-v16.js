66911d643960f977ae4e084881d3d6948d432834
Prof. Dr. Khaled Fouad Khaled <khaledrice2003@gmail.com>
2026-08-08 03:13:01 +0300

    Delete assets directory

--- assets/site-security-v16.js
+++
@@ -1,1 +0,0 @@
-(()=>{'use strict';document.querySelectorAll('a[target="_blank"]').forEach(a=>{const s=new Set((a.rel||'').split(/\s+/).filter(Boolean));s.add('noopener');s.add('noreferrer');a.rel=[...s].join(' ');});})();
